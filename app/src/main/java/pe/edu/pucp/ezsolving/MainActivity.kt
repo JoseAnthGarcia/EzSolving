2b7c4d227c0ac43bf3c404c3cec65b61c582cb8b
JohamRV <romuchovelasco3108>
2021-12-10 15:29:39 -0500

    no pullear - pner sus cambis sobre este comitt

--- app/src/main/java/pe/edu/pucp/ezsolving/MainActivity.kt
+++ app/src/main/java/pe/edu/pucp/ezsolving/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.view.WindowManager
 import pe.edu.pucp.ezsolving.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
