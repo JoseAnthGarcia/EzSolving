4f908429d6c41af4db9c66df1e45d7f702c2e96a
JohamRV <romuchovelasco3108>
2021-12-10 14:55:12 -0500

    bot´n flotante

--- app/src/main/java/pe/edu/pucp/ezsolving/ComentarioPublicacinActivity.kt
+++ app/src/main/java/pe/edu/pucp/ezsolving/ComentarioPublicacinActivity.kt
@@ -3,9 +3,9 @@ package pe.edu.pucp.ezsolving
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ComentarioPubliacionActivity : AppCompatActivity() {
+class ComentarioPublicacinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_comentario_publiacion)
+        setContentView(R.layout.activity_comentario_publicacin)
     }
 }
